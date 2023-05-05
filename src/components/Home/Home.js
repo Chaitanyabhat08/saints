963038137dc5e9fc8985ffaf1b96b80ec35434b7
Chaitanyabhat08 <Chaitanyabhat08@gmail.com>
2023-05-05 22:46:33 +0530

    added women categories card

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,19 +15,21 @@ const Home = () => {
   const alert = useAlert();
   const dispatch = useDispatch();
   const { loading, error, products, productCount } = useSelector(state => state.products);
-  const categories = [
+  const categoriesForHer = [
     {
-      name: "Tshirts", code: "Tshirt", images: [{ url: 'https://assets.ajio.com/medias/sys_master/root/20220907/KjI5/6317a235f997dd1f8de7f4c0/-473Wx593H-410326856-1510-MODEL.jpg'}]},
+      name: "Tshirts", code: "Tshirt", images: [{ url: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSr_I2tfnXHGIhtnzi3Zr8hwjX7b8jR7fnTNPirZrq-f3vnCgyWIOjGvl4vgSPMkN1yc_iLMhKeiUfmCeJfbny_P7reL9yFW0kpL4ctYsA&usqp=CAE' }], gender: 'F'
+    },
+    { name: "Sweatshirts", code: "SweatShirts", images: [{ url: 'https://cdn.shopify.com/s/files/1/0076/6040/4818/products/0-modelinfo-tamika-us2_eb86e6ac-da08-48eb-a415-c27396c051a1_450x610_crop_center.jpg?v=1674516904' }], gender: 'F' },
+    { name: "Shirts", code: "Shirts", images: [{ url: 'https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1683208757_2097830.jpg?format=webp&w=480&dpr=2.0' }], gender: 'F' },
     {
-      name: "Gadgets", code: "Gadgets", images: [{ url:'https://ksr-ugc.imgix.net/assets/028/214/522/b972ed4c1d4ed1cfc5d95b082331d529_original.jpg?ixlib=rb-4.0.2&w=680&fit=max&v=1582627824&gif-q=50&q=92&s=b8da14c658aa6c4692271d40f9a608ff'}] },
-    { name: "Sweatshirts", code: "SweatShirts", images: [{ url:'https://cdn.shopify.com/s/files/1/0076/6040/4818/products/0-modelinfo-tamika-us2_eb86e6ac-da08-48eb-a415-c27396c051a1_450x610_crop_center.jpg?v=1674516904'}] },
-    { name: "Shirts", code: "Shirts", images: [{ url:'https://cdn.shopify.com/s/files/1/0420/7073/7058/products/Amin4179_1800x1800.jpg?v=1672146549'}] },
-    // { name: "Pants", code: "Pants", images: [] },
-    // { name: "Sweatpants", code: "SweatPants", images: [] },
-    // { name: "Caps", code: "Capes", images: [] },
-    // { name: "Socks", code: "Socks", images: [] },
-    // { name: "Active Wear", code: "ActiveWear", images: [] }
-  ]
+      name: "Kurtis", code: "Kurtis", images: [{ url: 'https://cdn.shopify.com/s/files/1/0561/7926/1589/products/houseofchikankari-zainabmulprintedkurta-grey1_1800x1800.png?v=1678948318' }], gender: 'F' },
+  ];
+  const categoriesForHim = [{
+    name: "Sweatshirts", code: "SweatShirts", images: [{ url: 'https://divinebonds.in/wp-content/uploads/2022/09/oversized-hoodie-mockup-of-a-serious-couple-sitting-in-cubes-at-a-studio-m26223.jpg' }], gender: 'M'},
+    { name: "Shirts", code: "Shirts", images: [{ url: 'https://cdn.shopify.com/s/files/1/0420/7073/7058/products/Amin4179_1800x1800.jpg?v=1672146549' }], gender: 'M' },
+    { name: "Tshirts", code: "Tshirt", images: [{ url: 'https://cdn.shopify.com/s/files/1/0533/9578/3843/products/20220206_152500_900x.jpg?v=1673016320' }], gender: 'M' },
+    { name: "Pants", code: "Pants", images: [{ url: 'https://cdn.shopify.com/s/files/1/0752/6435/products/Layer8_6bb8e88c-6836-42bc-af3b-994b9a733be7_1452x1799.jpg?v=1661151328' }], gender: 'M' },
+  ];
   useEffect(() => {
     if (error) {
         alert.show(error);
@@ -40,7 +42,7 @@ const Home = () => {
         {loading ?
           <Fragment>
             <Loader />
-          </Fragment> : <Fragment>
+          </Fragment> :<> <Fragment>
           <MetaData title="Saints&Sinners"/>
             {/* <div><ImageCarousel images={images} /></div> */}
             <div><Banner /></div>
@@ -53,14 +55,24 @@ const Home = () => {
             </button>
               </a>
             </div>
- 
+          </Fragment>
+          <div className="categorySection">
             <h2 className="homeHeading">Featured Categories</h2>
-        <div className="container" id="container">
-          {categories.map(category => (
-            <CategoryCard category={category}/>
-          ))}
-        </div>
-      </Fragment>}
+            <div className="container1" id="container1">
+              <h3>For Him</h3>
+              {categoriesForHim.map(category => (
+                <CategoryCard category={category} />
+              ))}
+              </div>
+              <div className="container1" id="container1">
+                <h3>For Her </h3>
+                {categoriesForHer.map(category => (
+                  <CategoryCard category={category} />
+                ))}
+              </div>
+            </div>
+            </>
+      }
       </Fragment>
   )
 }
